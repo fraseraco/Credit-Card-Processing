2dcfad04a5331bbbbecb6000d01deb7734c0754d
Fraser Coyle <fraseraco@gmail.com>
2024-11-27 13:49:31 -0500

    Removed example code

--- TheTerminal/Display.c
+++ TheTerminal/Display.c
@@ -5,21 +5,6 @@
 #include <stdint.h>
 
 
-// int rc;
-// 	rc = lcd1602Init(1, 0x27); // init LCD display
-// 	if (rc)
-// 	{
-// 		printf("Initialization failed; aborting...\n");
-// 		return 0;
-//	}
-// lcd1602WriteString("String to LCD");
-// lcd1602SetCursor(0,1);
-// lcd1602WriteString("Write 2nd Line");
-// lcd1602Control(1,0,1); // backlight on, underline off, blink block on 
-// getchar(); // wait for character input to continue
-// lcd1602Shutdown(); 
-
-
 void DisplayMenu(char * str) {
     lcd1602WriteString("Select Mode");
     lcd1602SetCursor(0,1);
