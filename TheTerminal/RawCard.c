b571f0c54f486c0f03e591c8060ade912ae23b4c
Fraser Coyle <fraseraco@gmail.com>
2024-12-01 14:26:55 -0500

    Fixed getMagCard()

--- TheTerminal/RawCard.c
+++ TheTerminal/RawCard.c
@@ -10,10 +10,154 @@ int getReadMode(){
     return in;
 }
 
-CardInfo getMagData()
-{
-    CardInfo CC = NULL;
-    return CC;
+CardInfo getMagData() {
+    CardInfo cardInfo = malloc(sizeof(Card));
+    char card[256] = "";
+    char *token;
+    char cardNumber[16];
+    char month[3] = "";
+    char year[5] = "";
+    char *firstName = NULL;
+    char *lastName = NULL;
+    char *middle = NULL;
+    int flag = 0;
+    char *saveptr;
+    char *nameString;
+    char *nameToken;
+    
+    int cardYear = 0;
+    int cardMonth = 0;
+    int currentYear; 
+    int currentMonth;
+    struct tm* ptr;
+    time_t t;
+    t = time(NULL);
+    ptr = localtime(&t);
+    //print for debuggingCardInfo cardInfo;
+    printf("%s", asctime(ptr));
+
+    currentYear = ptr->tm_year + 1900;
+    currentMonth = ptr->tm_mon + 1;
+    
+    printf("Please swipe your card:\n");
+
+    
+    if (fgets(card, sizeof(card), stdin) != NULL) {
+      //  printf("Card data: %s\n", card);
+      
+        token = strtok_r(card, "^",&saveptr);
+        
+        while( token != NULL ){
+           
+            
+          if ( flag == 0 ) {
+            char *tempNum = token;
+        
+             //token is currently 5B/%b/%B + card number 
+            for ( int i = 2; i < strlen(tempNum); i ++ ){
+               
+               cardNumber[i-2] = tempNum[i];
+                 }
+              cardNumber[strlen(tempNum) - 2] = '\0';
+              printf("Card Number: %s\n", cardNumber);
+              cardInfo->cardNumber = cardNumber;
+            }
+            
+          else if ( flag == 1 ){
+
+            char temp[100];
+            strcpy(temp, token);
+         
+             strncpy(temp, token, sizeof(temp) - 1);
+                temp[sizeof(temp) - 1] = '\0';
+                nameString = strtok(temp, "/");  
+            
+            
+             
+              lastName = nameString; 
+                //print for debugging
+                cardInfo->lastName = lastName;
+                printf("Last Name: %s\n", lastName);
+               nameString = strtok(NULL, "/");
+                   nameToken = trim_whitespace(nameString);
+           
+            for ( int i = 0; i < strlen(nameToken) - 1; i ++){
+              
+                if( isspace( nameToken[i] ) ){
+                    char *firstToken = strtok( nameToken, " " );
+                
+                    while ( firstToken != NULL ){
+                       firstName = firstToken;
+                        firstToken = strtok(NULL, " ");
+                        
+                       middle = firstToken;
+
+                    firstToken = strtok( NULL, " " );
+                  
+                      }
+                  
+                }
+                else{
+                  firstName = nameToken;
+                  middle = "";
+                }
+               
+                
+               }
+               cardInfo->firstName = firstName;
+               cardInfo->middle = middle;
+                //print for debugging
+                printf("First Name: %s\n", firstName);
+                printf("Middle Name: %s\n", middle);
+                  
+              }
+
+       else if ( flag == 2 ){
+            year[0] = '2';
+            year[1] = '0';
+            year[2] = token[0];
+            year[3] = token[1];
+            year[4] = '\0';
+
+            month[0] = token[2];
+            month[1] = token[3];
+            month[2] = '\0';
+            
+            cardYear = atoi(year);
+            cardMonth = atoi(month);
+            cardInfo->cardYear = cardYear;
+            cardInfo->cardMonth = cardMonth;
+            
+            printf("Month: %d\n", cardMonth);
+            printf("Year: %d\n", cardYear);
+
+            }
+        
+         token = strtok_r(NULL, "^", &saveptr);
+         flag++;
+          
+        }
+        }  
+    
+    else {
+        printf("Failed to read card data.\n");
+    }
+
+    if ( cardYear < currentYear ){
+      printf("Card is expired, please present different form of payment\n");
+    }
+    else if( cardYear == currentYear && cardMonth < currentMonth ){
+      printf("Card is expired, please present different form of payment\n");
+     }
+    else{
+      printf("Processing...\n");
+    }
+    
+
+
+  
+  return cardInfo;
+
 }
 
 CardInfo getRFIDData()
